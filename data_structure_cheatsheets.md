a208637d1065559fcf87b6bb67a20d2226b4c2dc
Jethro Lai <jethro@jethrolai.com>
2018-06-10 00:06:39 -0700

    Add examples and data_Structure_cheatsheet.md

---
+++ data_structure_cheatsheets.md
@@ -0,0 +1,6 @@
+# Data Structure Cheatsheet
+
+| ADT | Implementation | Language | Note | Example |
+|------|---------------|----------|------|---------|
+| List | ArrayList       | Java      | get: O(1) |  |
+| List | LinkedList      | Java      | add: O(1) |  |
---
+++ examples/bash_shift.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# This example is to show you what shift command does in Bash
+# Run `./bash_shift.sh a b c d` or any arbitrary number of arguments you would like to pass in.
+
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+NO_COLOR='\033[0m' # No Color
+DIM='\033[2m'
+
+if [ "$#" -lt 1 ]; then
+  echo -e "${RED}No options provided. Try running \`./bash_shift.sh a b c d\` ."
+  exit 1
+fi
+
+echo -e "${BLUE}Printing each argument one at a time ...${YELLOW}"
+while [ -n "$1" ]
+do
+  echo -e "Current \$1: ${RED} $1"
+  echo -e "${DIM}${YELLOW}\tshifting variables ... ${NO_COLOR}${YELLOW}"
+  shift
+done
+echo -ne "${BLUE}"
+
+if [ -z "$1" ];then
+  echo '$1 is now blank'
+else
+  echo 'Unexpected. $1 is not blank'
+fi
